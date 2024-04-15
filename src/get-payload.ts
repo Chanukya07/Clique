c75007d4ded60e2f3b1c1cc3f8dbd5dda16999e6
BhaumikReddy <saibhaumikreddy@gmail.com>
2024-04-15 16:19:56 +0530

    completed auth flow

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -47,7 +47,7 @@ export const getPayloadClient = async ({
     cached.promise = payload.init({
       email: {
         transport: transporter,
-        fromAddress: 'onboarding@resend.dev',
+        fromAddress: 'clique@bhaumikreddy.online',
         fromName: 'Clique',
       },
       secret: process.env.PAYLOAD_SECRET,
