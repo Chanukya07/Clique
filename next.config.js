4fffca0928f24279405a6e47e935142e63b271bd
BhaumikReddy <saibhaumikreddy@gmail.com>
2024-04-18 11:37:09 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
 			},
 			{
 				protocol: "https",
-				hostname: "digitalhippo-production.up.railway.app",
+				hostname: "cliqueproject-production.up.railway.app",
 			},
 		],
 	},
